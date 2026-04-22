0cc9865d48908c51a245922025ed826a2e6fc723
saber037 <xrp654321@163.com>
2026-04-22 16:26:31 +0800

    Site updated: 2026-04-22 16:26:30

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2026/04/22/这是一篇新的博文/","2026/04/22/My-New-Post/","2026/04/22/hello-world/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
