15059a7ebedfc43ac0ae441ddec47f02db2bdc35
saber037 <xrp654321@gmail.com>
2026-04-24 17:45:21 +0800

    Site updated: 2026-04-24 17:45:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/04/24/这是一篇日志/","2026/04/24/这是一篇新的博文/","2026/04/24/hello-world/"];function toRandomPost(){
+var posts=["2026/04/24/Source Puzzle Game/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
