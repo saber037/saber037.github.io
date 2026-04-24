0c3978ea46734043a78feaa885fdcc3cdfe8536f
saber037 <xrp654321@gmail.com>
2026-04-24 18:57:48 +0800

    Site updated: 2026-04-24 18:57:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/04/24/Tetris/","2026/04/24/Minesweeper/","2026/04/24/Snake Game/","2026/04/24/Source Puzzle Game/"];function toRandomPost(){
+var posts=["2026/04/24/Tic Tac Toe/","2026/04/24/Flappy Bird/","2026/04/24/Breakout/","2026/04/24/Tetris/","2026/04/24/Minesweeper/","2026/04/24/Snake Game/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
